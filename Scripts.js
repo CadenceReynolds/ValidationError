84ccd5f2f714af151059a4cf45e0e8be372dca31
CadenceReynolds <reynocal001@warren.k12.in.us>
2025-01-28 12:06:37 -0500

    Initial commit

---
+++ Scripts.js
@@ -0,0 +1,19 @@
+class ValidationError extends Error{
+    constructor(message){
+        super(message)
+        this.name = ValidationError;
+    }
+}
+
+function validateUsername(username){
+    if(username.length > 10){
+        throw new ValidationError;
+    }
+    return "Username is Valid"
+}
+
+try{
+    console.log(validateUsername("abc"));
+}catch(error){
+    console.error(`${error.name}: ${error.message}`)
+}
